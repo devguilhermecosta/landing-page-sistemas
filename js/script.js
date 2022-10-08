6ee5355acac1394f92ad9aec46ed0be1a3ebb864
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-10-08 17:25:49 -0300

    alterei o código js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-var menu = document.getElementsByClassName('.menu-mobile')
-menu = addEventListener('click', relatar)
+var menu_m = document.getElementsByClassName('.menu-mobile')
+menu_m = addEventListener('click', activate)
 
-function relatar() {
-    window.alert('Teste')
+var menu = document.getElementById('menu')
+var state = false
+
+function activate() {
+
+    if (state == false) {
+        menu.style.display = "inline-block"
+        state = true
+    } else if (state == true) {
+        menu.style.display = "none"
+        state = false
+    }
 }
