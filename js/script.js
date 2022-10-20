dd03b3c8a2c6cd2c302dcf3ce9065bc76421aa72
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-10-20 12:47:32 -0300

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
 var mb = document.getElementById('mb')
 mb.addEventListener("click", activate)
 var menu = document.getElementById('menu')
+var altura = window.innerWidth
+var state = false
+
+if (altura >= 992) {
+    menu.style.display = "inline-block";
+}
 
 function activate() {
-    var state = false
     if (state == false) {
         menu.style.display = "inline-block";
-        mb.style.backgroundImage = "url(../images/close.png)"
+        // mb.style.backgroundImage = "url(../images/close.png)"
         state = true
     } else if (state == true) {
         menu.style.display = "none"
         mb.style.backgroundImage = "url(../images/menu.png)"
+        state = false
     }
 }
 
 function fechar() {
-    var altura = window.innerWidth
-    if (altura < 992) {
+    if (altura < 992 && state == true) {
         menu.style.display = "none"
         mb.style.backgroundImage = "url(../images/menu.png)"
+        state = false
     }
 }
