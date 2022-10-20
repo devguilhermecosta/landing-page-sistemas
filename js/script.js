e32011dcd33667a30e8e0f57b49a221e43466131
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-10-20 13:11:21 -0300

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,28 @@
 var mb = document.getElementById('mb')
 mb.addEventListener("click", activate)
 var menu = document.getElementById('menu')
-var altura = window.innerWidth
+var largura = window.innerWidth
 var state = false
-
-if (altura >= 992) {
-    menu.style.display = "inline-block";
-}
 
 function activate() {
     if (state == false) {
         menu.style.display = "inline-block";
-        // mb.style.backgroundImage = "url(../images/close.png)"
+        mb.style.backgroundImage = "url(../images/close.png)"
         state = true
     } else if (state == true) {
         menu.style.display = "none"
+        mb.style.display = "block"
         mb.style.backgroundImage = "url(../images/menu.png)"
         state = false
     }
 }
 
 function fechar() {
-    if (altura < 992 && state == true) {
+    if (largura < 992 && state == true) {
         menu.style.display = "none"
         mb.style.backgroundImage = "url(../images/menu.png)"
         state = false
+    } else if (largura >= 992) {
+        menu.style.display = "block"
     }
 }
