1e3a660da2e385deaaeb807e454faacdebb5e078
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-10-19 21:02:18 -0300

    adicionei JavaScript

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 var mb = document.getElementById('mb')
+mb.addEventListener("click", activate)
 var menu = document.getElementById('menu')
 var state = false
 
 function activate() {
-
     if (state == false) {
         menu.style.display = "inline-block"
         state = true
     } else if (state == true) {
+        menu.style.display = "none"
+        state = false
+    }
+}
+
+function fechar() {
+    if (state == true) {
         menu.style.display = "none"
         state = false
     }
