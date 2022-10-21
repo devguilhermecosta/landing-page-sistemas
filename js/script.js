d934703462cbe1d09ae1a6c0c68b3104141fdf89
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-10-20 23:25:04 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,19 +4,23 @@ var menu = document.getElementById('menu')
 var state = false
 
 function activate() {
-    if (state == false) {
-        menu.style.display = "inline-block"
-        mb.style.backgroundImage = "url('../images/close.png')"
-        state = true
-    } else if (state == true) {
-        menu.style.display = "none"
-        mb.style.backgroundImage = "url('../images/menu.png')"
-        state = false
+    if (window.innerWidth < 992) {
+        if (state == false) {
+            menu.style.display = "inline-block"
+            mb.style.backgroundImage = "url('../images/close.png')"
+            state = true
+        } else if (state == true) {
+            menu.style.display = "none"
+            mb.style.backgroundImage = "url('../images/menu.png')"
+            state = false
+        }
     }
 }
 
 function to_quit() {
-    menu.style.display = "none"
-    mb.style.backgroundImage = "url('../images/menu.png')"
-    state = false
+    if (window.innerWidth < 992) {
+        menu.style.display = "none"
+        mb.style.backgroundImage = "url('../images/menu.png')"
+        state = false
+    }
 }
