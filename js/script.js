6a3dbe94516a68942f7401017123ab57af3ba2b3
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-10-20 23:19:17 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
-var mb = document.getElementById('mb').addEventListener("click", activate)
+var mb = document.getElementById('mb')
+mb.addEventListener("click", activate)
 var menu = document.getElementById('menu')
 var state = false
 
 function activate() {
     if (state == false) {
         menu.style.display = "inline-block"
+        mb.style.backgroundImage = "url('../images/close.png')"
         state = true
     } else if (state == true) {
         menu.style.display = "none"
+        mb.style.backgroundImage = "url('../images/menu.png')"
         state = false
     }
 }
 
 function to_quit() {
     menu.style.display = "none"
+    mb.style.backgroundImage = "url('../images/menu.png')"
     state = false
 }
