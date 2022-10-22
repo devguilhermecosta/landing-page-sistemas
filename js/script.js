4fed8b5c4dc4854f417bf963482490fdf0c159b0
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-10-22 10:33:33 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
 var mb = document.getElementById('mb')
-mb.addEventListener("click", activate)
+mb.addEventListener("click", to_active)
+mb.style.backgroundImage = "url(images/menu.png)"
+mb.style.backgroundRepeat = "no-repeat"
+mb.style.backgroundColor = "transparent"
 var menu = document.getElementById('menu')
 var state = false
 
-function activate() {
-    if (window.innerWidth < 992) {
-        if (state == false) {
-            menu.style.display = "inline-block"
-            state = true
-        } else if (state == true) {
-            menu.style.display = "none"
-            state = false
-        }
+function to_active() {
+    if (state == false) {
+        menu.style.display = "block"
+        mb.style.backgroundImage = "url(images/close.png)"
+        state = true
+    } else if (state == true) {
+        menu.style.display = "none"
+        mb.style.backgroundImage = "url(images/menu.png)"
+        state = false
     }
 }
 
 function to_quit() {
     if (window.innerWidth < 992) {
         menu.style.display = "none"
+        mb.style.backgroundImage = "url(images/menu.png)"
         state = false
     }
 }
